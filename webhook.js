8c4b0155fe815889e9c6148b52006a6174de7d9e
Wesley <wesleyreid0@gmail.com>
2020-11-18 11:07:28 -0500

    Update webhook.js
    code cleanup

--- webhook.js
+++ webhook.js
@@ -1,14 +1,14 @@
 class Script {
 	prepare_outgoing_request({ request }) {
       	var content = request.data.text;
-	    var split_content = content.split("\n");
+	var split_content = content.split("\n");
         var form_content = split_content[0];
         var form_link = form_content.substring(7);
         var jobNum = split_content[2];
         var jobLink = split_content[3];
         var type = split_content[4];
         var customer = split_content[1];
-	    let x = Math.floor(Math.random() * 10000) + 1;
+	let x = Math.floor(Math.random() * 10000) + 1;
       	let msg = {
           text: type + ' Please click the link to view the parts order form for customer: ' + customer + " -- REF#" + x + "",
           attachments: [
